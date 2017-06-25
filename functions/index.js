f0762b4b6539ae6b252b28b5b2934b6e50da0977
Keerthi Niranjan <keerthiniranjan16@gmal.com>
2017-06-25 19:07:21 +0530

    Added public

--- functions/index.js
+++ functions/index.js
@@ -5,11 +5,7 @@ const functions = require('firebase-functions');
 
 exports.timeSheet = functions.https.onRequest((request, response) => {
     const app = new App({ request, response });
-    console.log('Request headers: ' + JSON.stringify(request.headers));
-    console.log('Request body: ' + JSON.stringify(request.body));
 
-    app.tell('Actually it looks like you heard it all. ' +
-        'Thanks for listening!');
     let actionMap = new Map();
     app.handleRequest(actionMap);
 });

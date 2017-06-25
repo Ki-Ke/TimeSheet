09bfb566bb5a96f8416f8df1c0e577f387bafee6
Keerthi Niranjan <keerthiniranjan16@gmal.com>
2017-06-25 15:20:40 +0530

    Testing first sample

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,15 @@
+'use strict';
+
+const App = require('actions-on-google').ApiAiApp;
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.timeSheet = functions.https.onRequest((request, response) => {
+    const app = new App({ request, response });
+    console.log('Request headers: ' + JSON.stringify(request.headers));
+    console.log('Request body: ' + JSON.stringify(request.body));
+
+    app.tell('Actually it looks like you heard it all. ' +
+        'Thanks for listening!');
+    let actionMap = new Map();
+    app.handleRequest(actionMap);
+});

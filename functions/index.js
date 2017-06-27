b4610b13ea55fdd22fbdbc8058bc3dd91581df85
Kiran Niranjan <kiran@kike.co.in>
2017-06-27 20:15:13 +0530

    Completed create project intent

--- functions/index.js
+++ functions/index.js
@@ -17,10 +17,19 @@
 
 const App = require('actions-on-google').ApiAiApp;
 const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
+
+firebase.initializeApp({
+    credential: firebase.credential.applicationDefault(),
+    databaseURL: "https://timesheet-2e733.firebaseio.com"
+});
+const db = firebase.database();
 
 // Api.ai intents
 const WELCOME_INTENT = 'input.welcome';
 const CREATE_PROJECT = 'input.createProject';
+const PROJECT_NAME_CONFIRMATION_YES = 'input.projectNameConfirmationYes';
+const PROJECT_NAME_CONFIRMATION_NO = 'input.projectNameConfirmationNo';
 
 // Time Sheet constants
 const appName = 'Time Sheet';
@@ -34,17 +43,58 @@ exports.timeSheet = functions.https.onRequest((request, response) => {
     }
 
     /**
+     * Method Related to creating a project
+     */
+
+    /**
      * Create a personal project
      * Ask user for the project name
      * @param app
      */
     function createProject(app) {
-        app.tell('Created project successful');
-        console.log("Created project success");
+        const projectName = app.getArgument('projectName');
+        app.askForConfirmation(`Are you sure you want to create a project with the name ${projectName}?`);
+    }
+
+    function projectNameConfirmationYes(app) {
+        const projectName = app.getArgument('projectName');
+        const userId = app.getUser().userId;
+
+        let user = db.ref('users/' + userId);
+        let promise = user.set({userId: userId});
+
+        let userProjects = db.ref('projects/' + userId);
+
+        userProjects.once("value")
+            .then(function(snapshot) {
+                let projectNameExists = false;
+                snapshot.forEach((childSnapshot) => {
+                    if (childSnapshot.val() === projectName){
+                        projectNameExists = true;
+                    }
+                });
+
+                if (projectNameExists){
+                    app.tell(`Project name already exists`);
+                } else {
+                    userProjects.push(projectName);
+                    app.tell(`Great! The project has been created. You can start logging right away! just say, Time sheet check in for ${projectName}`);
+                }
+            });
+    }
+
+    function projectNameConfirmationNo(app) {
+        app.tell(`That's okay. Let's not do it now.`);
     }
 
     const actionMap = new Map();
+    // Welcome intent
     actionMap.set(WELCOME_INTENT, welcome);
+
+    // Creating a project
     actionMap.set(CREATE_PROJECT, createProject);
+    actionMap.set(PROJECT_NAME_CONFIRMATION_YES, projectNameConfirmationYes);
+    actionMap.set(PROJECT_NAME_CONFIRMATION_NO, projectNameConfirmationNo);
+
     app.handleRequest(actionMap);
 });
